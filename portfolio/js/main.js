395996b3a121feb8f2ec66fb5886e45196b94ab3
byunhyesun <byeonyang@gmail.com>
2023-08-31 11:32:37 +0900

    test

---
+++ portfolio/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+	var notePad=$('.notepads')
+    $(window).scroll(function(){
+        if($(this).scrollTop()>300){
+            notePad.addClass('animate')
+        }else{
+            notePad.removeClass('animate')
+        }
+    })
+});
+
+
